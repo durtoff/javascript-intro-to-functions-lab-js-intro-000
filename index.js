7f40f06cee1905b843e3851492b65d9c321f0fd4
Duarte de Freitas <durtoff@gmail.com>
2019-07-19 09:37:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase) {
+  if (string === toLowerCase) {
     return = "I can't hear you!"
   }
-
 }
